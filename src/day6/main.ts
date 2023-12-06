084a1146e98c8f6f4427e972379d2c8e6dad832a
Evan Rutledge Borden <eborden@frontrowed.com>
2023-12-06 00:00:40 -0600

    Add solution 2 for Day 6

--- src/day6/main.ts
+++ src/day6/main.ts
@@ -1,16 +1,33 @@
 #!/usr/bin/env -S deno -L info run
 
 import { flushStdIn } from '../io.ts'
-import { raceParser } from './parser.ts'
+import { Race, raceParser } from './parser.ts'
 
 main()
 
 export async function main(): Promise<void> {
   const raw = await flushStdIn()
-  console.log(
-    raceParser(raw).map((race) =>
-      Array.from({ length: race.time }).map((_, i) => (race.time - i) * i)
-        .filter((x) => race.distance < x).length
-    ).reduce((x, y) => x * y, 1),
+  const races = raceParser(raw)
+  console.log('solution 1', races.map(numStrategies).reduce((x, y) => x * y, 1))
+
+  const strRace = races.reduce(
+    (acc, { time, distance }) => ({
+      time: acc.time + time,
+      distance: acc.distance + distance,
+    }),
+    { time: '', distance: '' },
   )
+  const longRace = {
+    time: parseInt(strRace.time, 10),
+    distance: parseInt(strRace.distance, 10),
+  }
+  console.log(
+    'solution 2',
+    numStrategies(longRace),
+  )
+}
+
+function numStrategies(race: Race): number {
+  return Array.from({ length: race.time }).map((_, i) => (race.time - i) * i)
+    .filter((x) => race.distance < x).length
 }
