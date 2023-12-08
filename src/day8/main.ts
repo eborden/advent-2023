4db475275a9a5eab8a0c7b55623965a708055e10
Evan Rutledge Borden <eborden@frontrowed.com>
2023-12-08 00:51:12 -0600

    Add draft of day 8.2

--- src/day8/main.ts
+++ src/day8/main.ts
@@ -8,24 +8,56 @@ main()
 export async function main(): Promise<void> {
   const raw = await flushStdIn()
   const map = mapParser(raw)
-  console.log('solution 1', await traverse('AAA', moves(map.path), map))
+
+  console.log(
+    'solution 1',
+    traverse(
+      [mkCursor('AAA')],
+      moves(map.path),
+      map,
+      (c) => c.position === 'ZZZ',
+    ),
+  )
+
+  const cursors = Object.keys(map.instructions)
+    .filter((x) => x[2] === 'A')
+    .map((position) => mkCursor(position))
+  console.log(
+    'solution 2',
+    traverse(cursors, moves(map.path), map, (c) => c.position[2] === 'Z'),
+  )
 }
 
-async function traverse(
-  position: string,
-  path: AsyncGenerator<Move, Move>,
+function traverse(
+  cursors: Cursor[],
+  path: Generator<Move, Move>,
   map: Map,
-): Promise<number> {
-  if (position === 'ZZZ') return 0
-  const { value: move } = await path.next()
-  return 1 + await traverse(map.instructions[position][move], path, map)
+  isFinish: (cursor: Cursor) => boolean,
+): number {
+  do {
+    const { value: move } = path.next()
+    cursors = cursors.map((cursor) => ({
+      position: map.instructions[cursor.position][move],
+      iteration: cursor.iteration + 1,
+    }))
+  } while (!cursors.every(isFinish))
+  return cursors[0].iteration
 }
 
-async function* moves(array: Move[]): AsyncGenerator<Move, Move> {
+function* moves(array: Move[]): Generator<Move, Move> {
   let i = 0
   while (true) {
     if (i == array.length) i = 0
     yield array[i]
     i++
   }
+}
+
+type Cursor = {
+  position: string
+  iteration: number
+}
+
+function mkCursor(position: string): Cursor {
+  return { position, iteration: 0 }
 }
